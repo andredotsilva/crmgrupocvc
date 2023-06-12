20382e9fffc7c25a0f98ce94f7e84ea5bea9e7cd
André Silva <asilvainfo@gmail.com>
2023-06-12 20:56:17 +0100

    GAS checkbox

--- resources/views/pages/contracts/create.blade.php
+++ resources/views/pages/contracts/create.blade.php
@@ -416,6 +416,29 @@
                                                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-gray-600">
                                             </div>
                                         </div>
+                                        <div class="sm:col-span-2">
+                                            <label for="super_off_peak"
+                                                class="block text-sm font-medium leading-6 text-gray-900 dark:text-gray-200">Gás</label>
+                                            <div class="flex">
+                                                <label class="inline-flex items-center mr-6">
+                                                  <input type="checkbox" class="form-checkbox">
+                                                  <span class="ml-2">1</span>
+                                                </label>
+                                                <label class="inline-flex items-center mr-6">
+                                                  <input type="checkbox" class="form-checkbox">
+                                                  <span class="ml-2">2</span>
+                                                </label>
+                                                <label class="inline-flex items-center mr-6">
+                                                  <input type="checkbox" class="form-checkbox">
+                                                  <span class="ml-2">3</span>
+                                                </label>
+                                                <label class="inline-flex items-center">
+                                                  <input type="checkbox" class="form-checkbox">
+                                                  <span class="ml-2">4</span>
+                                                </label>
+                                              </div>
+                                              
+                                        </div>
                                     </div>
                                 </div>
                                 <!--END Consumos-->
