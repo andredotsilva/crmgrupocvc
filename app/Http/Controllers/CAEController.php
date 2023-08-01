672df572ea02ebd8345858719d9cb98b0aa8acf1
Diogo <diogoteixeiralucas@proton.me>
2023-08-01 10:24:08 +0100

    options half done

--- app/Http/Controllers/CAEController.php
+++ app/Http/Controllers/CAEController.php
@@ -9,8 +9,54 @@ class CAEController extends Controller
 {
     public function index()
     {
-        $cnaefs = CAE::all();
+        $caes = CAE::all();
 
-        return response()->json($cnaefs, 200);
+        return response()->json($caes, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'code' => 'required',
+            'title' => 'required',
+        ]);
+
+        CAE::create([
+            'code' => $request->code,
+            'title' => $request->title,
+        ]);
+
+        return redirect()->route('')
+            ->with('success');
+    }
+
+    public function edit($id)
+    {
+        $cae = CAE::findOrFail($id);
+
+        return view('', [
+            $cae => '$cae'
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $cae = CAE::where('id', $id)->first();
+
+        $cae->code = $request->code;
+        $cae->title = $request->title;
+
+        $cae->save();
+
+        return redirect('')->with('success');
+    }
+
+    public function destroy($id)
+    {
+        $cae = CAE::findOrFail($id);
+
+        $cae->delete();
+
+        return redirect('')->with('');
     }
 }
