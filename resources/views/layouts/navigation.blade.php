db21c4ea6a539de4510532ff971fdf03a3a11a87
André Silva <asilvainfo@gmail.com>
2023-08-05 17:45:46 +0100

    lable fix nav
    Lembrei me que ele quer Inicio em vez de Dashboard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,7 +17,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Início') }}
                     </x-nav-link>
                 </div>
                 @if (!Auth::user()->is_client)
